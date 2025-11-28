ef57964d752fe4ef8bfa4d71ee02d1e624f8fb65
Miguel Batres <1373528+btrsco@users.noreply.github.com>
2025-11-28 05:23:32 -0700

    Wrap Bsky Labeler response arrays in Laravel Collections

--- src/Data/Responses/Bsky/Labeler/GetServicesResponse.php
+++ src/Data/Responses/Bsky/Labeler/GetServicesResponse.php
@@ -2,26 +2,26 @@
 
 namespace SocialDept\AtpClient\Data\Responses\Bsky\Labeler;
 
+use Illuminate\Support\Collection;
 use SocialDept\AtpSchema\Generated\App\Bsky\Labeler\Defs\LabelerView;
 use SocialDept\AtpSchema\Generated\App\Bsky\Labeler\Defs\LabelerViewDetailed;
 
 class GetServicesResponse
 {
     /**
-     * @param  array<LabelerView|LabelerViewDetailed>  $views
+     * @param  Collection<int, LabelerView|LabelerViewDetailed>  $views
      */
     public function __construct(
-        public readonly array $views,
+        public readonly Collection $views,
     ) {}
 
     public static function fromArray(array $data, bool $detailed = false): self
     {
         return new self(
-            views: array_map(
+            views: collect($data['views'] ?? [])->map(
                 fn (array $view) => $detailed
                     ? LabelerViewDetailed::fromArray($view)
-                    : LabelerView::fromArray($view),
-                $data['views'] ?? []
+                    : LabelerView::fromArray($view)
             ),
         );
     }
