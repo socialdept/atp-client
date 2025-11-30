b02b8a5daac9cc269a8a63f6927f053f06c79978
Miguel Batres <1373528+btrsco@users.noreply.github.com>
2025-11-30 06:26:49 -0700

    Add typed responses to IdentityRequestClient

--- src/Client/Requests/Atproto/IdentityRequestClient.php
+++ src/Client/Requests/Atproto/IdentityRequestClient.php
@@ -5,6 +5,8 @@ namespace SocialDept\AtpClient\Client\Requests\Atproto;
 use SocialDept\AtpClient\Attributes\PublicEndpoint;
 use SocialDept\AtpClient\Attributes\ScopedEndpoint;
 use SocialDept\AtpClient\Client\Requests\Request;
+use SocialDept\AtpClient\Data\Responses\Atproto\Identity\ResolveHandleResponse;
+use SocialDept\AtpClient\Data\Responses\EmptyResponse;
 use SocialDept\AtpClient\Enums\Nsid\AtprotoIdentity;
 use SocialDept\AtpClient\Enums\Scope;
 
@@ -16,14 +18,14 @@ class IdentityRequestClient extends Request
      * @see https://docs.bsky.app/docs/api/com-atproto-identity-resolve-handle
      */
     #[PublicEndpoint]
-    public function resolveHandle(string $handle): string
+    public function resolveHandle(string $handle): ResolveHandleResponse
     {
         $response = $this->atp->client->get(
             endpoint: AtprotoIdentity::ResolveHandle,
             params: compact('handle')
         );
 
-        return $response->json()['did'];
+        return ResolveHandleResponse::fromArray($response->json());
     }
 
     /**
@@ -34,11 +36,13 @@ class IdentityRequestClient extends Request
      * @see https://docs.bsky.app/docs/api/com-atproto-identity-update-handle
      */
     #[ScopedEndpoint(Scope::Atproto, granular: 'identity:handle')]
-    public function updateHandle(string $handle): void
+    public function updateHandle(string $handle): EmptyResponse
     {
         $this->atp->client->post(
             endpoint: AtprotoIdentity::UpdateHandle,
             body: compact('handle')
         );
+
+        return new EmptyResponse;
     }
 }
